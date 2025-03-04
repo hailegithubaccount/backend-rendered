4d10266544c9deb4a859df563861146c095b5b68
haile eshetu <michaelhaile1222@gmail.com>
2025-03-04 13:37:32 +0300

    adding some concept in termso tje getall request

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -5,7 +5,7 @@ const { protect,checkRole, checkUserExists} = require('../middleware/auth');  //
 const router = express.Router();
 
 router.post("/request",protect,checkRole("student"), checkUserExists,RequestController.requestBook);
-router.get("/request",RequestController. getAllBookRequests); // // Auth required
+router.get("/request",protect,checkRole("library-staff"), checkUserExists,RequestController. getAllBookRequests); // // Auth required
 router.patch("/approved/:requestId", protect, checkRole("library-staff"), checkUserExists, RequestController.approveBookRequest);
 
 module.exports = router;
