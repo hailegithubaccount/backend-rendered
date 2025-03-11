f0d8ec307217e5c61c177d2a7ff957994675513a
haile eshetu <michaelhaile1222@gmail.com>
2025-03-11 22:35:25 +0300

    this is the correction of the wishlist

--- model/wishlistModel.js
+++ model/wishlistModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const wishlistSchema = new mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-    book: { type: mongoose.Schema.Types.ObjectId, ref: 'books' },
+    student: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+     book: { type: mongoose.Schema.Types.ObjectId, ref: "books", required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
