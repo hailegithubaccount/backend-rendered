1a502132fa99b231a2a370153222dfc651cade0d
haile eshetu <michaelhaile1222@gmail.com>
2025-03-11 22:38:31 +0300

    adding correction

--- routes/WishlistRoute.js
+++ routes/WishlistRoute.js
@@ -5,7 +5,7 @@ const { protect,checkRole, checkUserExists} = require('../middleware/auth');
 
 
 
-router.get("/wishlist", protect, checkRole("library-staff"),checkUserExists, wishlistController.getWishlist);
+router.get("/wishlist", protect, wishlistController.getWishlist);
 router.post("/wishlist/:bookId", protect, checkRole("student"), checkUserExists, wishlistController.addToWishlist);
 
 
