cc7918c5d42f92384cbf8f7f70cd3ea8868cbf42
haile eshetu <michaelhaile1222@gmail.com>
2025-03-11 15:30:33 +0300

    adding the router in the wishlist routes

--- routes/WishlistRoute.js
+++ routes/WishlistRoute.js
@@ -7,3 +7,7 @@ const { protect,checkRole, checkUserExists} = require('../middleware/auth');
 
 router.get("/wishlist", protect, checkRole("library-staff"),checkUserExists, wishlistController.getWishlist);
 router.post("/wishlist/:bookId", protect, checkRole("student"), checkUserExists, wishlistController.addToWishlist);
+
+
+
+module.exports = router;
