db6f07017b86bdb121e6a63d8637a86a5f445776
haile eshetu <michaelhaile1222@gmail.com>
2025-04-13 15:58:24 +0300

    dd

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const statusController = require("../controller/AnnouncementController");
+const AnnouncementController = require("../controller/AnnouncementController")
 const { protect,checkRole, checkUserExists} = require('../middleware/auth'); // Ensure these are imported correctly
 
-router.post("/createAnnouncement",protect,checkRole("library-staff"), checkUserExists, statusController.createAnnouncement);
+router.post("/createAnnouncement",protect,checkRole("library-staff"), checkUserExists, AnnouncementController.createAnnouncement);
 router.get("/read",AnnouncementController.getAllAnnouncements);
-router.delete("/delete/:AnnouncementId",protect, checkRole("library-staff"), checkUserExists, statusController.deleteAnnouncement); 
+router.delete("/delete/:AnnouncementId",protect, checkRole("library-staff"), checkUserExists, AnnouncementController.deleteAnnouncement); 
 
 
 module.exports = router;
