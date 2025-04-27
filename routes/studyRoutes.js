e150308e2983dd53a73baf6e1ecbfef3dc2a6a7b
haile eshetu <michaelhaile1222@gmail.com>
2025-04-27 10:15:58 +0300

    adding the commi

--- routes/studyRoutes.js
+++ routes/studyRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const studyController = require('../controller/studyController');
+const { checkUserExists } = require('../middleware/auth');
 
 // You should protect these routes with auth middleware if needed
 
-router.post('/addChapter', studyController.addChapter);
-router.get('/getChapterPages/:chapterId', studyController.getChapterPages);
-router.post('/markPageRead', studyController.markPageRead);
-router.get('/getProgress/:chapterId', studyController.getProgress);
+router.post('/addChapter', protect, checkRole('student'), checkUserExists, studyController.addChapter);
+router.get('/getChapterPages/:chapterId', protect, checkRole('student'), checkUserExists, studyController.getChapterPages);
+router.post('/markPageRead', protect, checkRole('student'), checkUserExists, studyController.markPageRead);
+router.get('/getProgress/:chapterId', protect, checkRole('student'), checkUserExists, studyController.getProgress);
 
 module.exports = router;
