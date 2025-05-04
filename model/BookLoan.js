2b75ec24e3731c5061fe73ec1eab905d5e82ad16
haile eshetu <michaelhaile1222@gmail.com>
2025-05-04 16:09:06 +0300

    a

--- model/BookLoan.js
+++ model/BookLoan.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
   recipient: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: 'users',
     required: true
   },
   recipientEmail: {
