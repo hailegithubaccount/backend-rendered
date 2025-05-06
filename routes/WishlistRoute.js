ccb254f53122d180a8ec36c57abb671905b4fb9d
haile eshetu <michaelhaile1222@gmail.com>
2025-05-06 22:12:59 +0300

    adding into the wishlist

--- routes/WishlistRoute.js
+++ routes/WishlistRoute.js
@@ -5,7 +5,7 @@ const { protect,checkRole, checkUserExists} = require('../middleware/auth');
 
 
 
-router.get("/wishlist", protect, wishlistController.getWishlist);
+router.get("/wishlist/the", protect, wishlistController.getWishlist);
 router.post("/wishlist/:bookId", protect, checkRole("student"), checkUserExists, wishlistController.addToWishlist);
 
 //router.delete("/wishlist/:wishlistId", protect, checkRole("student"), checkUserExists, wishlistController.deleteFromWishlist);
